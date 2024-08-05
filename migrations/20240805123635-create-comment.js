c4b639173f2029989d43338bb14fa3baf791bbca
MungaiKeren <wambukeren@gmail.com>
2024-08-05 15:47:12 +0300

    Add Comment model and migration

---
+++ migrations/20240805123635-create-comment.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Comments', {
+      commentID: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      task_id: {
+        type: Sequelize.UUID,
+        allowNull: false
+      },
+      user_id: {
+        type: Sequelize.UUID,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Comments');
+  }
+};
---
+++ models/comment.js
@@ -0,0 +1,50 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Comment.belongsTo(models.Tasks, {
+        as: "task",
+        foreignKey: 'task_id',
+        onDelete: 'CASCADE'
+      });
+      Comment.belongsTo(models.User, {
+        as: "user",
+        foreignKey: 'user_id',
+        onDelete: 'CASCADE'
+      });
+    }
+  }
+  Comment.init({
+    commentID: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    task_id: {
+      type: DataTypes.UUID,
+      allowNull: false
+    },
+    user_id: {
+      type: DataTypes.UUID,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Comment',
+  });
+  return Comment;
+};
